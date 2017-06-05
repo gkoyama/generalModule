91988785829a37fc8b5436238e560e0194793816
小山剛 <g.koyama@leihauoli.com>
2017-06-05 15:31:33 +0900

    モジュールページver1.0完了の為コミット

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -21,7 +21,6 @@ LEIHAUOLI.MODULES.accordion = {
 				that.$jsiAccordionMenuTrigger.css('height',0);
 				that.$jsiAccordionMenuTrigger.css('display','block');
 				that.$jsiAccordionMenuTrigger.animate({height:that.accordionMenuHight},200,'linear',function(){
-					console.log('anmate OK!');
 				});
 			} else {
 				if(that.$jsiAccordionMenuTrigger.is('animated')){
@@ -29,7 +28,6 @@ LEIHAUOLI.MODULES.accordion = {
 				}
 				that.$jsiAccordionMenuTrigger.animate({height:0},200,'linear',function(){
 					that.$jsiAccordionMenuTrigger.css('display','none');
-					console.log('close OK!');
 				});
 			}
 		});
--- src/assets/js/smoothScroll.js
+++ src/assets/js/smoothScroll.js
@@ -1,40 +1,35 @@
-var LEIHAUOLI = LEIHAUOLI || {};
-LEIHAUOLI.MODULES = {};
-
 LEIHAUOLI.MODULES.smoothScroll = {
 	init : function($naviList){
 		this.$naviList = $naviList;
 		this.setParameters();
-		this.eventHandler();
+		this.landingPointStoring();
 	},
 	setParameters : function(){
 		this.$landingPoints = $('.jsi-section-head > h3');
-		// this.hrefReplace = this.$naviList.attr('href').replace('#','');
+		this.naviListHref = this.$naviList.attr('href').replace('#','');
 	},
-// 	eventHandler : function(){
-// 		var that = this;
-// 		that.$jsiAccordionTitleTrigger.on('click',function(){
-// 			if(that.$jsiAccordionMenuTrigger.is(':hidden')){
-// 				if(that.$jsiAccordionMenuTrigger.is(':animated')){
-// 					return;
-// 				}
-// 				that.$jsiAccordionMenuTrigger.css('height',0);
-// 				that.$jsiAccordionMenuTrigger.css('display','block');
-// 				that.$jsiAccordionMenuTrigger.animate({height:that.accordionMenuHight},200,'linear',function(){
-// 					console.log('anmate OK!');
-// 				});
-// 			} else {
-// 				if(that.$jsiAccordionMenuTrigger.is('animated')){
-// 					return;
-// 				}
-// 				that.$jsiAccordionMenuTrigger.animate({height:0},200,'linear',function(){
-// 					that.$jsiAccordionMenuTrigger.css('display','none');
-// 					console.log('close OK!');
-// 				});
-// 			}
-// 		});
-// 	}
-// };
+	landingPointStoring : function(){
+		var that = this;
+		this.$landingPoints.each(function(){
+			var landingPoint = $(this).attr('id');
+			if (landingPoint == that.naviListHref){
+				var landingPointOffset = $(this).offset();
+				that.landingPointTop = landingPointOffset.top;
+				that.eventHandler(that.landingPointTop);
+			}
+		});
+	},
+	eventHandler : function(scrollPointTop){
+		var that = this;
+		that.$naviList.on('click',function(event){
+			event.preventDefault();
+			if ( $('html, body').is(':animated') ) {
+				return;
+			}
+			$('html, body').animate({scrollTop:scrollPointTop},500);
+		});
+	}
+};
 
 $(function(){
 	$('li > a').each(function(){
